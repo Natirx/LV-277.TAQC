bee64f30b02afb90e96ac1ae6900668b7d36d881
Natirx <triko2020@gmail.com>
2017-11-12 21:29:12 +0200

    Added module and modified class

--- Abramove/224.rb
+++ Abramove/224.rb
@@ -1,41 +1,15 @@
-class PRime
-    
-        def initialize(num)
-            @num = num
-            @result =[]
-            @uniq =[]
-            @b = (2..@num)
-        end
-        
-        def divisors
-             @b.each do |n| 
-             if @num%n==0
-                 @result.push(n)
-             end
-            end
-             @result
-        end
-    
-        def prime
-            
-              @result.each do |num|
-                @uniq << num
-                 (2...num).each do |v|
-                if num % v == 0
-                    @uniq.delete(num)
-              end
-            end
-        end
-    end
+require "./module.rb"
+class Primee
+  include PrimeModule
 
-def show_prime
-    p "Prime divisors : #{@uniq}" 
-    @uniq.clear
-    @result.clear
-end 
-def show_divisors
-    p "Divisors: #{@result}"
-   
+  attr_reader :num, :result
+
+  def initialize(num)
+    @num = num
+    @result = []
+  end
 end
-    end
- 
+
+prime = Primee.new(12)
+p prime.divisors
+p prime.prime_of_array
