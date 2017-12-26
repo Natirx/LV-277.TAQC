21b8936a7dcbe94907c2616cb1b32f999531fb8d
yaroslav <garasym@gmail.com>
2017-12-26 13:15:45 +0200

    Allure exemple

---
+++ allure_reports.bat
@@ -0,0 +1,4 @@
+@echo off
+allure serve reports
+
+rem download server from https://bintray.com/qameta/generic/allure2
--- spec/allure_demo_spec.rb
+++ spec/allure_demo_spec.rb
@@ -3,24 +3,29 @@ describe "MySpec", :feature => "Some feature", :severity => :normal do
 #describe "MySpec" do
 
   before(:step) do |s|
-    puts "Before step #{s.current_step}"
+    puts "*****Before step #{s.current_step}"
   end
 
   it "should be critical", :story => "First story", :severity => :critical, :testId => 99 do
   #it "should be critical" do
+    puts "*****it Compare"
     "string".should == "string"
   end
 
   it "should be steps enabled", :story => ["First story", "Second story"], :testId => 31 do |e|
+    
     e.step "step1" do |s|
-      s.attach_file "screenshot1", take_screenshot_as_file
+      puts "*****step1"
+      #s.attach_file "screenshot1", take_screenshot_as_file
     end
 
     e.step "step2" do
+      puts "*****step2"
       5.should be > 0
     end
 
     e.step "step3" do
+      puts "*****step2"
       0.should == 0
     end
 
