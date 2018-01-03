00bb651f30d7528609eaf26eac1b50aacfbd77cd
DenysKvyk <denyskvyk@gmail.com>
2018-01-03 01:54:29 -0800

    add Aplication

--- spec/test.rb
+++ spec/test.rb
@@ -1,11 +1,12 @@
+require_relative '../pages/application.rb'
 
 describe "open_cart" do
-    context "given home_page" do
-      it "add to cart" do
-        expect(Application.get.load_home_page
-                   .add_2)
-        sleep 5
-      end
+  context "given home_page" do
+    it "add to cart" do
+      expect(Application.get.load_home_page
+             .add_2)
+      sleep 5
     end
+  end
 
 end
