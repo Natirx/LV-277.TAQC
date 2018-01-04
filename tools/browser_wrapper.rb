097fa6f8a107cea6d2f15ffdf407d9b6c6bb88c5
Danylo Soroka <dsoroktc@training.local>
2018-01-04 13:06:10 +0200

    changed browser wrapper

--- tools/browser_wrapper.rb
+++ tools/browser_wrapper.rb
@@ -7,24 +7,36 @@ class BrowserWrapper
 
   private
 
+  # def init_browser(application_source)
+  #   # TODO Use factory method
+  #   if application_source.browserName.include?("chrome")
+  #     Selenium::WebDriver::Chrome.driver_path = application_source.driverPath
+  #     @driver = Selenium::WebDriver.for :chrome
+  #     #
+  #     # Selenium::WebDriver::Chrome.driver_path = application_source.driverPath
+  #     options = Selenium::WebDriver::Chrome::Options.new
+  #     options.add_argument('--headless')
+  #     options.add_argument('--disable-gpu')
+  #     @driver = Selenium::WebDriver.for :chrome, options: options
+  #   end
+  #   if application_source.browserName.include?("firefox")
+  #     Selenium::WebDriver::Firefox.driver_path = application_source.driverPath
+  #     @driver = Selenium::WebDriver.for :firefox
+  #   end
+  #   # TODO Move to strategy classes
+  #   @driver.manage.timeouts.implicit_wait = application_source.implicitWaitTimeOut
+  # end
+
   def init_browser(application_source)
+    Selenium::WebDriver::Chrome.driver_path = application_source.driverPath
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     # TODO Use factory method
-    if application_source.browserName.include?("chrome")
-      Selenium::WebDriver::Chrome.driver_path = application_source.driverPath
-      @driver = Selenium::WebDriver.for :chrome
-      #
-      Selenium::WebDriver::Chrome.driver_path = application_source.driverPath
-      options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--headless')
-      options.add_argument('--disable-gpu')
-      @driver = Selenium::WebDriver.for :chrome, options: options
-    end
-    if application_source.browserName.include?("firefox")
-      Selenium::WebDriver::Firefox.driver_path = application_source.driverPath
-      @driver = Selenium::WebDriver.for :firefox
-    end
+    @driver = Selenium::WebDriver.for :chrome, options: options
     # TODO Move to strategy classes
     @driver.manage.timeouts.implicit_wait = application_source.implicitWaitTimeOut
+    #@driver.manage.window.maximize
   end
 
   public
