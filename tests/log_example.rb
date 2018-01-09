74665223308318cf88860fdb937afae1f45df4e4
yaroslav <garasym@gmail.com>
2018-01-09 14:02:00 +0200

    read from yaml; example

--- tests/log_example.rb
+++ tests/log_example.rb
@@ -1,6 +1,28 @@
 require 'rubygems'
 require 'logging'
+require 'yaml'
+require 'pathname'
 
+# Dir.chdir(File.dirname(__FILE__))
+puts "from YAML ..."
+#puts Dir.pwd
+
+config_options = YAML.load_file("../config/example.yaml")
+#config_options = { 'source_and_target_cols_map' =>
+# [  { 'com_id' => 'community_id',
+#    'report_dt' => 'note_date',
+#    'sitesection' => 'site_section',
+#    'visitor_cnt' => 'visitors',
+#    'visit_cnt' => 'visits',
+#    'view_cnt' => 'views',
+#    'new_visitor_cnt' => 'new_visitors' }
+#  ]}
+
+  config_options['source_and_target_cols_map'][0].each do |key,value|
+  puts "key= #{key},  value= #{value}"
+end
+ 
+puts "from LIST ..."
 list = [0,1,2,3,4,5]
 #puts list[1..5]
 puts list[1..-1]
